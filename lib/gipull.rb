7446065e827e43ff2f9c24d2432d9ae6f9cf6af2
zaru <sakuraba@basicinc.jp>
2016-06-07 14:06:41 +0900

    add sample usage config

--- lib/gipull.rb
+++ lib/gipull.rb
@@ -1,11 +1,39 @@
 require "gipull/version"
+require "gipull/config"
 require "thor"
 
 module Gipull
   class CLI < Thor
+
+    class_options [ :config ]
+
+    def initialize(*args)
+      super
+      @config = Gipull::Config.instance
+    end
+
     desc "hello NAME", "say hello to NAME"
     def hello(name)
-      puts "Hello #{name}"
+      puts "Hello #{name} #{@config.data}"
+    end
+
+    desc "list ORG/REPO", "List pull-requests"
+    def list(org_repo)
+      init unless @config.access_token
+
+      say "これがプルリクだ！　#{org_repo} #{colored_message}"
+    end
+
+    desc "init", "Initialized gipull"
+    def init
+      token = ask 'AccessToken:'
+      @config.access_token = token
+    end
+
+    no_commands do
+      def colored_message
+        set_color "レビュー待ち", :white, :on_red, :bold
+      end
     end
   end
 end
